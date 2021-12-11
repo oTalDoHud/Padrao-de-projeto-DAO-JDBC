74e049e76e544189c719b64eeb3ebfac016e0dbc
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-11 10:15:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department depar = new Department(1, "Books");
 		
-		System.out.println(depar);
+		Seller seller = new Seller(21, "Maria", "Maria@gmail.com", new Date(), 3000.0, depar);
+		
+		System.out.println(seller);
+	
 	}
-
 }
