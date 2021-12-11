95622e2161f287e1ebcb6ea0aaba6f9294f9d185
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-11 10:29:32 -0300

    SellerDaoJdbc e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department depar = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Maria", "Maria@gmail.com", new Date(), 3000.0, depar);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	
