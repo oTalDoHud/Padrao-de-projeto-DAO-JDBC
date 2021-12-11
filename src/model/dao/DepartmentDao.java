f0877bb1da37f809c1058ab74225aed2648bfa3a
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-11 10:21:32 -0300

    Criando interfaces DAO

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert (Department depar);
+	
+	void update (Department depar);
+	
+	void deleteById (Integer id);
+	
+	Department findById (Integer id);
+	
+	List<Department> findAll ();
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+void insert (Seller depar);
+	
+	void update (Seller depar);
+	
+	void deleteById (Integer id);
+	
+	Seller findById (Integer id);
+	
+	List<Seller> findAll ();
+
+}
